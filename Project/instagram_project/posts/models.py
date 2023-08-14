f324d969ca234c70854116be91aecae1c6a4e11f
abhishekyadav0888 <shraddhayadav0888.com>
2023-08-14 19:20:57 +0530

    Done

--- Project/instagram_project/posts/models.py
+++ Project/instagram_project/posts/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
-    username=models.CharField(max_length=40)
-    caption=models.TextField()
-    createdAt=models.DateTimeField(auto_now_add=True)
+    username = models.CharField(max_length=100)
+    caption = models.TextField()
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.username
+        return self.caption[:20]
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text[:20]
--- Project/instagram_project/posts/urls.py
+++ Project/instagram_project/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_post,view_posts,DeletePost
+from . import views
 
-urlspatterns= [
-    path('create/',create_post,name='create-post'),
-    
-    path('view/',view_posts,name='view-posts'),
-    path('delete/<int:post_id>/',DeletePost,name='delete-post'),
+urlpatterns = [
+    path('create/', views.create_post, name='create_post'),
+    path('view/', views.view_posts, name='view_posts'),
+    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('like/<int:post_id>/', views.like_post, name='like_post'),
+    path('comment/<int:post_id>/', views.add_comment, name='add_comment'),
 ]
